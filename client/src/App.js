e83f7a7c6ce14ea0182f45d391bdaac99fbdfc13
Astra <abhaypratapsinghwd@gmail.com>
2023-12-25 10:33:30 +0530

    Added Routes --Pagination

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,25 @@
 import Home from "./components/Home/Home";
 import Auth from "./components/Auth/Auth";
 import Navbar from "./components/Navbar/Navbar";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
+import PostDetails from "./components/PostDetails/PostDetails";
+import { useSelector } from "react-redux";
 
 function App() {
+  const user = useSelector((state) => state.auth.authState);
   return (
     <div>
       <Navbar />
       <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route exact path="/auth" element={<Auth />} />
+        <Route exact path="/" element={<Navigate to="/posts" />} />
+        <Route exact path="/posts" element={<Home />} />
+        <Route exact path="/posts/search" element={<Home />} />
+        <Route exact path="/posts/:id" element={<PostDetails />} />
+        <Route
+          exact
+          path="/auth"
+          element={user ? <Navigate to="/posts" /> : <Auth />}
+        />
       </Routes>
     </div>
   );

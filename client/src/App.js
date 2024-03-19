b66bb767dfe3f643b695e436586eac3458693da2
Astra <abhaypratapsinghwd@gmail.com>
2024-03-19 16:32:59 +0530

    Initial SetUp

--- client/src/App.js
+++ client/src/App.js
@@ -1,48 +1,24 @@
-import Home from "./components/Home/Home";
-import Auth from "./components/Auth/Auth";
-import Navbar from "./components/Navbar/Navbar";
-import PostDetails from "./components/PostDetails/PostDetails";
-import { Routes, Route, Navigate } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import { ThemeProvider } from "@mui/material/styles";
-import theme from "./theme";
-import Notification from "./components/Notification/Notification";
-import { hideNotification } from "./actions/notifications";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const { open, message, severity } = useSelector(
-    (state) => state.notification
-  );
-  const handleCloseNotification = () => {
-    dispatch(hideNotification());
-  };
-
-  const user = useSelector((state) => state.auth.authState);
-  const dispatch = useDispatch();
-
   return (
-    <ThemeProvider theme={theme}>
-      <Navbar />
-      <Routes>
-        <Route exact path="/" element={<Navigate to="/posts" />} />
-        <Route exact path="/posts" element={<Home />} />
-        <Route exact path="/posts/search" element={<Home />} />
-        <Route exact path="/posts/:id" element={<PostDetails />} />
-        <Route
-          exact
-          path="/auth"
-          element={user ? <Navigate to="/posts" /> : <Auth />}
-        />
-      </Routes>
-      <Notification
-        open={open}
-        onClose={() => {
-          handleCloseNotification();
-        }}
-        message={message}
-        severity={severity}
-      />
-    </ThemeProvider>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
--- server/routes/user.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { signIn, signUp } = require("../controllers/user");
-
-router.post("/signIn", signIn);
-router.post("/signUp", signUp);
-
-module.exports = router;

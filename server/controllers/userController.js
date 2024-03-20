8996b31e080e0973ee7ab4120f24557fc77c4e44
Astra <abhaypratapsinghwd@gmail.com>
2024-03-20 08:35:37 +0530

    JWT Token

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,10 +1,16 @@
 const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+
+const createToken = (_id) => {
+  return jwt.sign({ _id }, process.env.SECRET_KEY, { expiresIn: "3d" });
+};
 
 const signUpController = async (req, res) => {
   try {
     const { email, password } = req.body;
     const user = await User.signUp(email, password);
-    res.status(201).json({ message: "User Created", user });
+    const token = createToken(user._id);
+    res.status(201).json({ message: "User Created", token });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }

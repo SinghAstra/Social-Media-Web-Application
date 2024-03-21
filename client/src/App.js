6dbbb90f86af1a7e5813b671c3b0a8f2b8dd34f4
Astra <abhaypratapsinghwd@gmail.com>
2024-03-21 15:17:57 +0530

    Added Routes to Sidebar

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,30 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import LogIn from "./pages/LogIn";
 import SignUp from "./pages/SignUp";
 import Footer from "./components/Footer";
 import Welcome from "./components/Welcome";
+import AddPost from "./components/AddPost";
+import Profile from "./components/Profile";
+import Friends from "./components/Friends";
+import Settings from "./components/Settings";
 
 function App() {
   return (
     <div className="bg-black text-white font-mono">
       <Navbar />
       <Routes>
+        <Route path="/" element={<Navigate to="/app/welcome" />} />
         <Route path="app" element={<Home />}>
           <Route path="/app/welcome" element={<Welcome />} />
+          <Route path="/app/add-post" element={<AddPost />} />
+          <Route path="/app/profile" element={<Profile />} />
+          <Route path="/app/friends" element={<Friends />} />
+          <Route path="/app/settings" element={<Settings />} />
         </Route>
-        <Route path="/log-in" element={<LogIn />} />
-        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/auth/log-in" element={<LogIn />} />
+        <Route path="/auth/sign-up" element={<SignUp />} />
       </Routes>
       <Footer />
       {/* <img
--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -4,43 +4,53 @@ import AddCircleOutlinedIcon from "@mui/icons-material/AddCircleOutlined";
 import {
   PeopleAltOutlined,
   PersonOutline,
-  PersonOutlineTwoTone,
   SettingsOutlined,
 } from "@mui/icons-material";
+import { Link } from "react-router-dom";
 
 const Sidebar = () => {
   return (
     <div className="sidebar-container h-screen">
       <ul className="flex flex-col gap-1 p-1">
         <li>
-          <div className="flex items-center gap-2 p-2 hover:bg-blue-400 cursor-pointer">
-            <HomeOutlinedIcon style={{ fontSize: 30 }} />
-            <h2 className="text-xl ">Home</h2>
-          </div>
+          <Link to={"/app/welcome"}>
+            <div className="flex items-center gap-2 p-2 hover:bg-blue-400 cursor-pointer">
+              <HomeOutlinedIcon style={{ fontSize: 30 }} />
+              <h2 className="text-xl ">Home</h2>
+            </div>
+          </Link>
         </li>
         <li>
-          <div className="flex items-center gap-2 p-2  hover:bg-blue-400 cursor-pointer">
-            <AddCircleOutlinedIcon style={{ fontSize: 30 }} />
-            <h2 className="text-xl ">Add Post</h2>
-          </div>
+          <Link to={"/app/add-post"}>
+            <div className="flex items-center gap-2 p-2  hover:bg-blue-400 cursor-pointer">
+              <AddCircleOutlinedIcon style={{ fontSize: 30 }} />
+              <h2 className="text-xl ">Add Post</h2>
+            </div>
+          </Link>
         </li>
         <li>
-          <div className="flex items-center gap-2 p-2  hover:bg-blue-400 cursor-pointer">
-            <PersonOutline style={{ fontSize: 30 }} />
-            <h2 className="text-xl ">Profile</h2>
-          </div>
+          <Link to="/app/profile">
+            <div className="flex items-center gap-2 p-2  hover:bg-blue-400 cursor-pointer">
+              <PersonOutline style={{ fontSize: 30 }} />
+              <h2 className="text-xl ">Profile</h2>
+            </div>
+          </Link>
         </li>
         <li>
-          <div className="flex items-center gap-2 p-2  hover:bg-blue-400 cursor-pointer">
-            <PeopleAltOutlined style={{ fontSize: 30 }} />
-            <h2 className="text-xl ">Friends</h2>
-          </div>
+          <Link to="/app/friends">
+            <div className="flex items-center gap-2 p-2  hover:bg-blue-400 cursor-pointer">
+              <PeopleAltOutlined style={{ fontSize: 30 }} />
+              <h2 className="text-xl ">Friends</h2>
+            </div>
+          </Link>
         </li>
         <li>
-          <div className="flex items-center gap-2 p-2  hover:bg-blue-400 cursor-pointer">
-            <SettingsOutlined style={{ fontSize: 30 }} />
-            <h2 className="text-xl ">Settings</h2>
-          </div>
+          <Link to="/app/settings">
+            <div className="flex items-center gap-2 p-2  hover:bg-blue-400 cursor-pointer">
+              <SettingsOutlined style={{ fontSize: 30 }} />
+              <h2 className="text-xl ">Settings</h2>
+            </div>
+          </Link>
         </li>
       </ul>
     </div>
